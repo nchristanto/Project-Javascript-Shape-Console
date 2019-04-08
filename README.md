2db017d86f041c4460d44aa8c2116abe7cee9b3d
nchristanto <christanto.nanda@gmail.com>
2019-04-08 20:21:19 +0700

    initialize index.js, index.html, README.md, .gitignore

---
+++ README.md
@@ -0,0 +1,5 @@
+# Project Javascript Shape Console
+
+Javascript experiment: 
+- Calculate areas of circle using Javascript
+- Calculate circumference of circe using Javascript
---
+++ index.js
@@ -0,0 +1,10 @@
+const pi = 3.14159;
+const radius = 3; 
+
+//area of circle 
+const circleArea = pi * radius ** 2
+console.log(circleArea)
+
+//circumference of circle
+const circleCircumference = 2 * pi * radius
+console.log(Math.round(circleCircumference))
